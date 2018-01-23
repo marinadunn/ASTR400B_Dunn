04671019727be47f6b06317757e961f651b71e11
marinadunn <marinadunn@email.arizona.edu>
2018-01-23 14:41:41 -0700

    Added solution in light years

--- Homework2/ParticleProperties.py
+++ Homework2/ParticleProperties.py
@@ -6,6 +6,7 @@
 
 #First, import Numpy and Astropy modules
 import numpy as np
+import astropy.units as u
 from ReadFile import Read
 
 #defining a function that will read in inputs and calculate the 3D distance, velocity, and mass of particle
@@ -14,19 +15,15 @@ def ParticlePropertyInfo(ptype, pnum, filename):
     #filename is the file from which we are importing data, in this case, MW_000.txt
 
     #This calls the function to read in these 3 data types
-    #print 'printing full output:', Read(filename)
-    #time = Read(filename)
-    #total_particles = Read(filename)
-    #data = Read(filename)
     time, total_particles, data = Read(filename)
 
     #optional print statement to see if inputs were read in properly
     print "time in Gyr = %s" %time
     print "total number of particles = %s" %total_particles
-    #print data['type']==0
+    
     #We want to create an array that stores the index of particle types
     index = np.where(data['type']==ptype)
-    #print index
+    
     #Here we are assigning mass, position, and velocity for the Nth particle
     
     mnew = data['m'][index]
@@ -37,14 +34,13 @@ def ParticlePropertyInfo(ptype, pnum, filename):
     vynew = data['vy'][index]
     vznew = data['vz'][index]
     
-    #print xnew
     #calculates the 3D distance and velocity
     #For particle p_num, the array index is p_num -1
     distance = np.sqrt(xnew[pnum-1]**2 + ynew[pnum-1]**2 + znew[pnum-1]**2)*u.kpc
     velocity = np.sqrt(vxnew[pnum-1]**2 + vynew[pnum-1]**2 + vznew[pnum-1]**2)*u.kpc
     distance_new = np.around(distance,3)
     velocity_new = np.around(velocity,3)
-    m = mnew[pnum-1]*1e10
+    m = mnew[pnum-1]*1e10*u.solMass
 
     return distance_new, velocity_new, m
 
@@ -57,4 +53,8 @@ print "The 3D distance of the particle %s of type %s in kpc is: " %(pnum,ptype),
 print "The 3D velocity of the particle %s of type %s in kpc is: " %(pnum,ptype), results[1]
 print "The mass of the particle %s of type in Solar Masses is:", results[2]
 
+distance_Ly = results[0].to(u.lyr)
+velocity_Ly = results[1].to(u.lyr)
 
+print "The 3D distance of the particle %s of type %s in ly is: " %(pnum,ptype), distance_Ly
+print "The 3D velocity of the particle %s of type %s in ly is: " %(pnum,ptype), velocity_Ly
