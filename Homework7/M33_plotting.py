bd846fd637202d79c0c5655971b8a38d21357f66
marinadunn <marinadunn@email.arizona.edu>
2018-03-29 17:00:50 -0700

    Fixed plotting script

--- Homework7/M33_plotting.py
+++ Homework7/M33_plotting.py
@@ -1,14 +1,15 @@
 # Created by Marina Dunn on 3/29/18.
 # ASTR400B Spring 2018, Prof. Gurtina Besla
 
+
 # import plotting modules
 import matplotlib.pyplot as plt
 import matplotlib
-
+import numpy as np
 
 M31Orbit = np.genfromtxt('M31_Orbit.txt',dtype=None,names=True)
 M33Orbit = np.genfromtxt('M33_Orbit.txt',dtype=None,names=True)
-M33Analytic = np.genfromtxt('M33Analytic_orbit.txt',dtype=None,names=True)
+M33Analytic = np.genfromtxt('M33Analytical_orbit.txt',dtype=None,names=True)
 
 M33Simulation_separation = np.sqrt((M33Orbit['x']-M31Orbit['x'])**2+(M33Orbit['y']-M31Orbit['y'])**2+(M33Orbit['z']-M31Orbit['z'])**2)
 M33Analytic_separation = np.sqrt((M33Analytic['x'])**2+(M33Analytic['y'])**2+M33Analytic['z']**2)
@@ -16,7 +17,7 @@ M33Analytic_separation = np.sqrt((M33Analytic['x'])**2+(M33Analytic['y'])**2+M33
 #First plot M33-M31 position comparison
 
 #plot figure
-plt.figure(1,figsize=(10,10))
+figure1 = plt.figure(1,figsize=(10,10))
 
 #plot the simulation vs. analytic predictions
 plt.plot(M31Orbit['t'], M33Simulation_separation, c='o', label='Besla Simulation')
@@ -32,10 +33,15 @@ plt.ylabel('Separation (kpc)', fontsize=18)
 #Add plot legend
 plt.legend()
 
+#save plot
+plt.savefig('M33-M31_Separation.jpg')
+plt.close()
+
+
 #Next plot M33-M31 velocity comparison
 
 #plot figure
-plt.figure(1,figsize=(10,10))
+figure2 = plt.figure(1,figsize=(10,10))
 
 #plot the simulation vs. analytic predictions
 plt.plot(M31Orbit['t'], M33Simulation_separation, c='o', label='Besla Simulation')
@@ -50,3 +56,7 @@ plt.ylabel('Velocity (km/s)', fontsize=18)
 
 #Add plot legend
 plt.legend()
+
+#save plot
+plt.savefig('M33-M31_Velocity.jpg')
+plt.close()
